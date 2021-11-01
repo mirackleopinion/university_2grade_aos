028f6e192cd17a0c3b67e41e4bd681e20f672102
mirackleopinion <90701233+mirackleopinion@users.noreply.github.com>
2021-11-01 08:47:25 +0200

    TC

--- lab2_emul/main.py
+++ lab2_emul/main.py
@@ -14,7 +14,7 @@ def tick():
 
 def prepare(_p, cmd):
     _p['IR'] = cmd
-    _p['TC'] += 1
+    _p['TC'] = 1
 
 
 def to_register(s):
@@ -68,7 +68,7 @@ def execute(_p):
         set_ps(_p, dst)
 
     _p['TC'] += 1
-    _p['PC'] += 1
+    _p['PC'] = 2
 
 
 def set_ps(_p, dst):
