f7b2c5a5b98e56989cca417f51dd4084ae0548d6
Дошик Терешков < @DESKTOP-FGI2GNG>
2021-11-14 12:59:46 +0200

    fix

--- lab3_fpu/main.py
+++ lab3_fpu/main.py
@@ -3,7 +3,8 @@
 STACK_SIZE = 8
 MANTISS_WIDTH = 10
 EXPONENT_WIDTH = 10
-
+WIDTH = 1 + EXPONENT_WIDTH + MANTISS_WIDTH
+EXP_SHIFT = 2 ** (EXPONENT_WIDTH - 1)
 
 stack = []
 
@@ -20,8 +21,32 @@ def pop():
     return stack.pop()
 
 
-def float_to_ieee754():
-    x = float(pop())
+def str_to_ieee754(a):
+    # TODO sign
+    if '.' in a:
+        a1, a2 = a.split('.')
+    else:
+        a1, a2 = a, '0'
+    _divider = 10 ** len(a2)
+
+    a1, a2 = int(a1), int(a2)
+
+    _before = []
+    while a1:
+        _before.insert(0, a1 % 2)
+        a1 = a1 // 2
+
+    _after = []
+    while a2 and len(_after) < width:
+        a2 *= 2
+        _after.append(a2 // _divider)
+        a2 %= _divider
+
+    # TODO +-0
+
+    print(f"{''.join(map(str, _before))}.{''.join(map(str, _after))}")
+
+    # TODO IEEE
 
 
 def ieee754_to_str():
