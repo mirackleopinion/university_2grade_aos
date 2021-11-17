4b71fcf1dc5f4cbbdbf2445b5f17c74d479b933b
Дошик Терешков < @DESKTOP-FGI2GNG>
2021-11-17 22:28:45 +0200

    sin cos * /

--- lab3_fpu/main.py
+++ lab3_fpu/main.py
@@ -4,6 +4,7 @@
 # 9 явних біти у мантисі плюс знаковий біт;
 # 9 варіант із таблиці формул для обрахунку співпроцесором.
 # 7*x*(sin x/cos y)
+from math import sin, cos
 
 STACK_SIZE = 8
 stack = []
@@ -21,18 +22,8 @@ EXPONENT_WIDTH = 7
 
 WIDTH = 1 + EXPONENT_WIDTH + MANTISS_WIDTH
 EXP_SHIFT = 2 ** (EXPONENT_WIDTH - 1) - 1
-
-
-def push(x):
-    if len(stack) == STACK_SIZE:
-        raise ValueError("stack overflow")
-    stack.append(x)
-
-
-def pop():
-    if len(stack) == 0:
-        raise ValueError("stack is empty")
-    return stack.pop()
+MAXIMUM_EXP = 2 ** EXPONENT_WIDTH - 1 - EXP_SHIFT
+MINIMUM_EXP = -EXP_SHIFT
 
 
 def set_exp(arr, exp):
@@ -42,6 +33,76 @@ def set_exp(arr, exp):
         arr[pos] = exp % 2
         pos -= 1
         exp = exp // 2
+
+
+def is_zero(x):
+    pass  # TODO
+
+
+def is_infinity(x):
+    pass  # TODO
+
+
+def is_nan(x):
+    pass  # TODO
+
+
+def is_denormalized(x):
+    pass  # TODO
+
+
+def ieee754_to_float(x):
+    S, E, M = ieee754_cutter(x)
+    if is_denormalized(x):
+        return ((-1) ** S) * (2 ** E) * (M / (2 ** MANTISS_WIDTH))
+    else:
+        return ((-1) ** S) * (2 ** E) * (1 + M / (2 ** MANTISS_WIDTH))
+
+
+def float_to_ieee754(x):
+    return str_to_ieee754(f'{x:.15f}')
+
+
+def ieee754_cutter(x):
+    s = x[0]
+
+    e = 0
+    for i in range(1, EXPONENT_WIDTH + 1):
+        e *= 2
+        e += x[i]
+
+    e -= EXP_SHIFT
+
+    m = 0
+    for i in range(EXPONENT_WIDTH + 1, WIDTH):
+        m *= 2
+        m += x[i]
+
+    return s, e, m
+
+
+def ieee754_to_str(x):
+    result = []
+    result.append(str(x[0]))  # sign
+    result.append(''.join(map(str, x[1:EXPONENT_WIDTH + 1])))  # exponenta
+    result.append(''.join(map(str, x[EXPONENT_WIDTH + 1:])))  # mantissa
+
+    if is_zero(x):
+        if x[0] == 1:
+            result.append('-0.0')
+        else:
+            result.append('+0.0')
+    elif is_infinity(x):
+        if x[0] == 1:
+            result.append('-∞')
+        else:
+            result.append('+∞')
+    elif is_nan(x):
+        result.append('NaN')
+    else:
+        result.append(f'{ieee754_to_float(x):.20f}')
+
+    return ' '.join(result)
 
 
 def str_to_ieee754(a):
@@ -68,9 +129,9 @@ def str_to_ieee754(a):
         _before.insert(0, a1 % 2)
         a1 = a1 // 2
 
-    if len(_before) > EXP_SHIFT:  # +/- inf
+    if len(_before) > MAXIMUM_EXP:  # +/- inf
         result[0] = sign
-        set_exp(result, 2 ** EXPONENT_WIDTH - 1 - EXP_SHIFT)
+        set_exp(result, MAXIMUM_EXP)
     else:
         _after = []
 
@@ -106,21 +167,119 @@ def str_to_ieee754(a):
                 for i in range(min(len(_after) - exponent, MANTISS_WIDTH)):
                     result[1 + EXPONENT_WIDTH + i] = _after[exponent + i]
 
-        print(f"{'-' if sign else '+'}{''.join(map(str, _before))}.{''.join(map(str, _after))} ")
+    return result
+
+
+def show_stack():
+    print('Stack:')
+    for i in reversed(range(STACK_SIZE)):
+        print(f'{i}: {ieee754_to_str(stack[i]) if i < len(stack) else "-"}')
+    print()
+
+
+def fpush(x, verbose=True):
+    if len(stack) == STACK_SIZE:
+        raise ValueError("stack overflow")
+
+    stack.append(x)
+
+    if verbose:
+        print(f'Push: {ieee754_to_str(x)}')
+        show_stack()
+
+
+def fpop(verbose=True):
+    if len(stack) == 0:
+        raise ValueError("stack is empty")
+
+    result = stack.pop()
+
+    if verbose:
+        print(f'Pop: {ieee754_to_str(result)}')
+        show_stack()
 
     return result
 
 
-def ieee754_to_str():
-    pass
+def fmul():
+    a = fpop(verbose=False)
+    b = fpop(verbose=False)
+
+    if is_nan(a) or is_nan(b):
+        raise ValueError('Multiplication to NaN')
+
+    if is_infinity(a) or is_infinity(b):
+        raise ValueError('Multiplication to infinity')
+
+    c = float_to_ieee754(ieee754_to_float(a) * ieee754_to_float(b))
+    fpush(c, verbose=False)
+    print(f'fmul {ieee754_to_float(a)} * {ieee754_to_float(b)} = {ieee754_to_float(c)} ')
+    show_stack()
 
 
-def add():
-    x = pop()
-    y = pop()
-    z = x + y  # TODO
-    push(z)
+def fdiv():
+    a = fpop(verbose=False)
+    b = fpop(verbose=False)
 
-# print(str_to_ieee754('-0.0000000000000000000005'))
-print(str_to_ieee754('155.625'))
-# print(str_to_ieee754('-0.05'))
+    if is_nan(a) or is_nan(b):
+        raise ValueError('Divide with NaN')
+
+    if is_infinity(a) or is_infinity(b):
+        raise ValueError('Divide with infinity')
+
+    if is_zero(b) or is_denormalized(b):
+        raise ZeroDivisionError('Divide with zero or denormalized')
+
+    c = float_to_ieee754(ieee754_to_float(a) / ieee754_to_float(b))
+    fpush(c, verbose=False)
+    print(f'fdiv {ieee754_to_float(a)} / {ieee754_to_float(b)} = {ieee754_to_float(c)} ')
+    show_stack()
+
+
+def fsin():
+    x = fpop(verbose=False)
+
+    if is_nan(x):
+        raise ValueError('sin(NaN)')
+
+    if is_infinity(x):
+        raise ValueError('sin(infinity)')
+
+    c = float_to_ieee754(sin(ieee754_to_float(x)))
+    fpush(c, verbose=False)
+    print(f'fsin sin({ieee754_to_float(x)}) = {ieee754_to_float(c)} ')
+    show_stack()
+
+
+def fcos():
+    x = fpop(verbose=False)
+
+    if is_nan(x):
+        raise ValueError('cos(NaN)')
+
+    if is_infinity(x):
+        raise ValueError('cos(infinity)')
+
+    c = float_to_ieee754(cos(ieee754_to_float(x)))
+    fpush(c, verbose=False)
+    print(f'fsin cos({ieee754_to_float(x)}) = {ieee754_to_float(c)} ')
+    show_stack()
+
+
+x = input("x:")
+y = input("y:")
+
+fpush(str_to_ieee754(y))    # y
+fcos()                      # cos(y)
+fpush(str_to_ieee754('7'))  # 7                       | cos(y)
+fpush(str_to_ieee754(x))    # x                       | 7      | cos(y)
+fmul()                      # x * 7                   | cos(y)
+fpush(str_to_ieee754(x))    # x                       | x * 7  | cos(y)
+fsin()                      # sin(x)                  | x * 7  | cos(y)
+fmul()                      # sin(x) * x * 7          | cos(y)
+fdiv()                      # sin(x) * x * 7 / cos(y)
+
+print('Checking:')
+f_x = float(x)
+f_y = float(y)
+print(7.0 * f_x * sin(f_x) / cos(f_y))
